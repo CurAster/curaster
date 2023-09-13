ef20693448df7a62a29ca28e711a7aa91a577b84
shivamkumarsingh28 <saeeamsingh@gmail.com>
2023-09-13 15:24:36 +0530

    curaster website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Cred-logo.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Cur Aster Website Comming Soon.
         </p>
         <a
           className="App-link"
